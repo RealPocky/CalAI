3dd96aa30f07f608ba6c586a2ea6f235f313628b
Pocky <mickycoma@gmail.com>
2026-05-17 05:43:37 +0700

    fix cache icon

--- app/+html.tsx
+++ app/+html.tsx
@@ -9,8 +9,8 @@ export default function Root({ children }: PropsWithChildren) {
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="theme-color" content="#4CAF50" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <link rel="icon" href="/apple-touch-icon.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon-v2.png" />
+        <link rel="icon" href="/apple-touch-icon-v2.png" />
         <ScrollViewStyleReset />
       </head>
       <body>{children}</body>
